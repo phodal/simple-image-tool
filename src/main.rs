87913bfd3d7228607e2997d93be05957931b06c5
Phodal Huang <h@phodal.com>
2021-08-19 20:03:31 +0800

    feat: add basic watermark support

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,7 @@ pub struct AppState {
     pub fill_strat: FillStrat,
     pub title: String,
     pub files: Vec<String>,
+    pub watermark: String,
     pub messages: Vec<String>,
     pub status: String,
 }
@@ -61,6 +62,18 @@ impl AppState {
                 if ext == "jpg" || ext == "png" || ext == "jpeg" {
                     log::info!("add file: {:?}", path.display());
                     self.files.push(format!("{}", path.display()));
+                }
+            }
+        }
+    }
+
+    pub fn set_watermark(&mut self, path: Arc<Path>) {
+        match path.extension() {
+            None => {}
+            Some(result) => {
+                let ext = format!("{}", result.to_str().unwrap());
+                if ext == "png" {
+                    self.watermark = format!("{}", path.display());
                 }
             }
         }
@@ -94,31 +107,42 @@ impl Data for AppState {
     }
 }
 
+pub const WATERMARK: Selector = Selector::new("simple.watermark");
 pub const PROCESSING: Selector = Selector::new("simple.processing");
+pub const OPENING: Selector = Selector::new("simple.opening");
 pub const DONE: Selector = Selector::new("simple.done");
 pub const MESSAGE: Selector<String> = Selector::new("simple.message");
 
 fn button() -> impl Widget<AppState> {
     Flex::row()
         .with_child(Button::new("Open").on_click(|ctx, _data: &mut AppState, _env| {
+            ctx.submit_command(OPENING);
             ctx.submit_command(Command::new(
                 druid::commands::SHOW_OPEN_PANEL,
                 FileDialogOptions::new(),
                 Target::Auto,
             ))
         }))
+        .with_child(Button::new("设置水印").on_click(|ctx, _data: &mut AppState, _env| {
+            ctx.submit_command(WATERMARK);
+            ctx.submit_command(Command::new(
+                druid::commands::SHOW_OPEN_PANEL,
+                FileDialogOptions::new(),
+                Target::Auto,
+            ));
+        }))
         .with_default_spacer()
         .with_child(
-        Button::new("Convert").on_click(|ctx, data: &mut AppState, _env| {
-            ctx.submit_command(PROCESSING);
-            for file in data.files.clone() {
-                resize_image(file.clone());
-                ctx.submit_command(MESSAGE.with(format!("done: {:?}", file.clone())));
-                &data.remove_file(file);
-            }
-            ctx.submit_command(DONE);
-        })
-    )
+            Button::new("Convert").on_click(|ctx, _data: &mut AppState, _env| {
+                ctx.submit_command(PROCESSING);
+                for file in data.files.clone() {
+                    resize_image(file.clone(), &data.watermark);
+                    ctx.submit_command(MESSAGE.with(format!("done: {:?}", file.clone())));
+                    &data.remove_file(file);
+                }
+                ctx.submit_command(DONE);
+            })
+        )
 }
 
 fn make_ui() -> impl Widget<AppState> {
@@ -131,34 +155,55 @@ fn make_ui() -> impl Widget<AppState> {
         .background(LIGHTER_GREY)
 }
 
-fn resize_image(file: String) {
+fn resize_image(file: String, watermark: &str) {
     let path = Path::new(&file);
     let img = image::open(&file).unwrap();
     log::info!("origin size: {}x{}", img.width(), img.height());
     let resize_height: f32 = img.height() as f32 / img.width() as f32 * 3072.0;
     let resize_width = 3072;
     log::info!("resize size: {}x{}", resize_width, resize_height);
-    let scale = img.resize(resize_width, resize_height as u32, FilterType::Nearest);
+    let mut scale = img.resize(resize_width, resize_height as u32, FilterType::Nearest);
 
     let parent = path.parent().unwrap();
     let file_name = path.file_name().unwrap();
     let prefix = parent.join(file_name);
     let new_file_name = format!("{}-thumb.jpg", prefix.display());
     let mut output = File::create(&new_file_name).unwrap();
-    scale.write_to(&mut output, ImageFormat::Jpeg).unwrap();
+
+    if watermark != "" {
+        let wm_image = image::open(&Path::new(watermark)).ok().expect("Opening image failed");
+        image::imageops::overlay(&mut scale, &wm_image, 100, 20);
+        scale.write_to(&mut output, ImageFormat::Jpeg).unwrap();
+    } else {
+        scale.write_to(&mut output, ImageFormat::Jpeg).unwrap();
+    }
 }
 
 #[derive(Debug, Default)]
 pub struct Delegate;
 
 impl AppDelegate<AppState> for Delegate {
-    fn command<'a>(&mut self, _ctx: &mut DelegateCtx<'a>, _target: Target, cmd: &Command, data: &mut AppState, _env: &Env) -> Handled {
+    fn command<'a>(&mut self, ctx: &mut DelegateCtx<'a>, _target: Target, cmd: &Command, data: &mut AppState, _env: &Env) -> Handled {
         if let Some(info) = cmd.get(druid::commands::OPEN_FILE) {
+            println!("{:?}", data.status);
+            if data.status == "watermark" {
+                data.set_watermark(Arc::from(info.path().to_owned()));
+                ctx.submit_command(MESSAGE.with(format!("watermark: {:?}", data.watermark.clone())));
+                return Handled::Yes;
+            }
             data.add_file(Arc::from(info.path().to_owned()));
             return Handled::Yes;
         }
         if let Some(_) = cmd.get(PROCESSING) {
             data.set_status("processing");
+            return Handled::Yes;
+        }
+        if let Some(_) = cmd.get(OPENING) {
+            data.set_status("opening");
+            return Handled::Yes;
+        }
+        if let Some(_) = cmd.get(WATERMARK) {
+            data.set_status("watermark");
             return Handled::Yes;
         }
         if let Some(_) = cmd.get(DONE) {
@@ -187,6 +232,7 @@ pub fn main() {
         fill_strat: FillStrat::Cover,
         title: "".to_string(),
         files: vec![],
+        watermark: "".to_string(),
         messages: vec![],
         status: "".to_string(),
     };
