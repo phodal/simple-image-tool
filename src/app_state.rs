3e562da9c977c8bb8d2a3559fb42e4e1c67189ae
Phodal Huang <h@phodal.com>
2021-08-23 08:32:06 +0800

    refactor: move resize images file

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,8 +1,14 @@
+use std::fs::File;
 use std::path::Path;
 use std::sync::Arc;
 
-use druid::{Data, Lens};
+use druid::{Data, EventCtx, Lens};
+use druid::image::{GenericImageView, ImageFormat};
+use druid::image::imageops::FilterType;
 use druid::widget::FillStrat;
+
+use crate::IMAGE_MAX_WIDTH;
+use crate::sit_delegate::MESSAGE;
 
 #[derive(Clone, Lens)]
 pub struct AppState {
@@ -53,6 +59,14 @@ impl AppState {
         let index = self.files.iter().position(|x| *x == file).unwrap();
         self.files.remove(index);
     }
+
+    pub fn process_files(&mut self, ctx: &mut EventCtx) {
+        for file in self.files.clone() {
+            resize_image(file.clone(), &self.watermark);
+            ctx.submit_command(MESSAGE.with(format!("done: {:?}", file.clone())));
+            &self.remove_file(file);
+        }
+    }
 }
 
 impl Data for AppState {
@@ -67,4 +81,40 @@ impl Data for AppState {
             .zip(other.files.iter())
             .all(|(a, b)| a.same(b))
     }
+}
+
+fn resize_image(file: String, watermark: &str) {
+    let path = Path::new(&file);
+    let origin_image = image::open(&file).unwrap();
+    log::info!("origin size: {}x{}", origin_image.width(), origin_image.height());
+
+    let mut scale;
+    if origin_image.width() < IMAGE_MAX_WIDTH || origin_image.height() < IMAGE_MAX_WIDTH {
+        scale = origin_image;
+        log::info!("image width or height < {:?}", IMAGE_MAX_WIDTH);
+    } else {
+        let resize_height: f32 = origin_image.height() as f32 / origin_image.width() as f32 * 3072.0;
+        let resize_width = IMAGE_MAX_WIDTH;
+        log::info!("resize size: {}x{}", resize_width, resize_height);
+        scale = origin_image.resize(resize_width, resize_height as u32, FilterType::Nearest);
+    }
+
+    let new_file_name = thumb_output_path(path);
+    let mut output = File::create(&new_file_name).unwrap();
+
+    if watermark != "" {
+        let wm_image = image::open(&Path::new(watermark)).ok().expect("Opening image failed");
+        image::imageops::overlay(&mut scale, &wm_image, 100, 20);
+        scale.write_to(&mut output, ImageFormat::Jpeg).unwrap();
+    } else {
+        scale.write_to(&mut output, ImageFormat::Jpeg).unwrap();
+    }
+}
+
+fn thumb_output_path(path: &Path) -> String {
+    let parent = path.parent().unwrap();
+    let file_name = path.file_name().unwrap();
+    let prefix = parent.join(file_name);
+    let new_file_name = format!("{}-thumb.jpg", prefix.display());
+    new_file_name
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
 extern crate image;
 
-use std::fs::File;
-use std::path::Path;
-
 use druid::{AppLauncher, Color, Command, Target, Widget, WidgetExt, WindowDesc};
-use druid::{
-    FileDialogOptions
-};
+use druid::FileDialogOptions;
 use druid::widget::{Button, FillStrat, Flex};
-use image::{GenericImageView, ImageFormat};
-use image::imageops::FilterType;
 
 use app_state::AppState;
 use components::gallery::Gallery;
 use components::message_box::MessageBox;
-use sit_delegate::{DONE, MESSAGE, OPENING, PROCESSING, SitDelegate, WATERMARK};
+use sit_delegate::{DONE, OPENING, PROCESSING, SitDelegate, WATERMARK};
 
 const LIGHTER_GREY: Color = Color::rgb8(242, 242, 242);
 
@@ -25,7 +18,7 @@ pub mod sit_menu;
 
 fn button() -> impl Widget<AppState> {
     Flex::row()
-        .with_child(Button::new("Open").on_click(|ctx, _data: &mut AppState, _env| {
+        .with_child(Button::new("打开文件").on_click(|ctx, _data: &mut AppState, _env| {
             ctx.submit_command(OPENING);
             ctx.submit_command(Command::new(
                 druid::commands::SHOW_OPEN_PANEL,
@@ -44,13 +37,9 @@ fn button() -> impl Widget<AppState> {
         }))
         .with_default_spacer()
         .with_child(
-            Button::new("Convert").on_click(|ctx, data: &mut AppState, _env| {
+            Button::new("转换").on_click(|ctx, data: &mut AppState, _env| {
                 ctx.submit_command(PROCESSING);
-                for file in data.files.clone() {
-                    resize_image(file.clone(), &data.watermark);
-                    ctx.submit_command(MESSAGE.with(format!("done: {:?}", file.clone())));
-                    &data.remove_file(file);
-                }
+                data.process_files(ctx);
                 ctx.submit_command(DONE);
             })
         )
@@ -67,42 +56,6 @@ fn make_ui() -> impl Widget<AppState> {
 }
 
 const IMAGE_MAX_WIDTH: u32 = 3072;
-
-fn resize_image(file: String, watermark: &str) {
-    let path = Path::new(&file);
-    let origin_image = image::open(&file).unwrap();
-    log::info!("origin size: {}x{}", origin_image.width(), origin_image.height());
-
-    let mut scale;
-    if origin_image.width() < IMAGE_MAX_WIDTH || origin_image.height() < IMAGE_MAX_WIDTH {
-        scale = origin_image;
-        log::info!("image width or height < {:?}", IMAGE_MAX_WIDTH);
-    } else {
-        let resize_height: f32 = origin_image.height() as f32 / origin_image.width() as f32 * 3072.0;
-        let resize_width = IMAGE_MAX_WIDTH;
-        log::info!("resize size: {}x{}", resize_width, resize_height);
-        scale = origin_image.resize(resize_width, resize_height as u32, FilterType::Nearest);
-    }
-
-    let new_file_name = thumb_output_path(path);
-    let mut output = File::create(&new_file_name).unwrap();
-
-    if watermark != "" {
-        let wm_image = image::open(&Path::new(watermark)).ok().expect("Opening image failed");
-        image::imageops::overlay(&mut scale, &wm_image, 100, 20);
-        scale.write_to(&mut output, ImageFormat::Jpeg).unwrap();
-    } else {
-        scale.write_to(&mut output, ImageFormat::Jpeg).unwrap();
-    }
-}
-
-fn thumb_output_path(path: &Path) -> String {
-    let parent = path.parent().unwrap();
-    let file_name = path.file_name().unwrap();
-    let prefix = parent.join(file_name);
-    let new_file_name = format!("{}-thumb.jpg", prefix.display());
-    new_file_name
-}
 
 pub fn main() {
     let title = "Hug8217";
